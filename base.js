c850a34b0909a0a1bd31f87b6754ff82c1be43a1
dettac <345098184@qq.com>
2016-10-28 23:50:38 +0800

    去掉alert

--- base.js
+++ base.js
@@ -318,13 +318,13 @@ $(function () {
     							  imgUrl: 'http://cdn.ohao.ren/image/weui/icon_nav_library.png',
     							  trigger: function (res) {
     							    // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-    							    alert('用户点击分享到朋友圈');
+    							    //alert('用户点击分享到朋友圈');
     							  },
     							  success: function (res) {
     							    alert('感谢您的分享');
     							  },
     							  cancel: function (res) {
-    							    alert('已取消');
+ 
     							  },
     							  fail: function (res) {
     							    alert(JSON.stringify(res));
